e87de013bd6f54bf3d524d4ab38ccce8b0636272
yahel <yahel020@gmail.com>
2021-11-11 19:06:19 +0200

    change server.py

--- clientEx2.1.py
+++ clientEx2.1.py
@@ -3,7 +3,7 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('127.0.0.1', 12345))
-s.send(b'Yahel Jacoby 208384420 Yoav Tamir 316554724')
+s.send(b'new connection')
 data = s.recv(100)
 print("Server sent: ", data)
 s.close()
--- server.py
+++ server.py
@@ -1,13 +1,70 @@
 import socket
+import random
+import string
+
+
+
+
+
+
+def new_connect():
+    id = id_generator()
+    dict[id] = None
+    return id
+
+
+def new_inf(id, data):
+    for client in dict:
+        if client is not id:
+            dict.update({client, client + data})
+
+
+# TODO ask about import rendom and string
+def id_generator():
+    return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(128))
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 12345))
 server.listen(5)
+dict = {}
+
 while True:
     client_socket, client_address = server.accept()
     print('Connection from: ', client_address)
-    data = client_socket.recv(100)
-    print('Received: ', data)
-    # client_socket.send(data.upper())
+    data = bytes('', 'utf-8')
+    while len(data) < 100000:
+        temp = client_socket.recv(1024)
+        data = data + temp
+        if temp == b'':
+            break
+
+    #data = client_socket.recv(100)
+    if data == b'new connection':
+        id = new_connect()
+
+        client_socket.send(bytes(id, "utf-8"))
+    else:
+        id, flag, path = data.decode('utf-8').split("|")
+
+        if flag == "receive":
+            if dict.get(id) is not None:
+                client_socket.send(bytes(dict.get(id), "utf-8"))
+
+            dict.update({id, None})
+        else:
+
+            new_inf(id, data)
+            print('Received: ', data)
+            if dict.get(id) is not None:
+                client_socket.send(bytes(dict.get(id), "utf-8"))
+
+            dict.update({id, None})
+
+
+
+    #client_socket.send(data.upper())
+
     client_socket.close()
     print('Client disconnected')
+
